7af0a706cce0bb108d1d7c72d618bbede118c12a
Fernweh99 <daniele.dimento@gmail.com>
2022-06-23 15:15:05 +0200

    finish I bonus

--- bonus1/js/script.js
+++ bonus1/js/script.js
@@ -5,6 +5,18 @@ e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
+/*
+BONUS 1: Crea un elemento contenitore nel DOM e aggiungendo 
+(attraverso il metodo append(), appendChild() o innerHTML)
+i vari elementi html con il numero o la stringa corretta da mostrare.
+*/
+
+//Recupero l'elemento block-container dal DOM
+myContainerContent = document.getElementById("block-container");
+
+//creo una variabile per stampare il contenuto in pagina
+let content = "<ul>";
+
 //creo ciclo per creare i numeri e farci validazioni e operazioni
 for (let i = 1 ; i <= 100 ; i++) {
   const number = i;
@@ -22,6 +34,10 @@ for (let i = 1 ; i <= 100 ; i++) {
     outputNumber = "Buzz";
   }
 
-  console.log(outputNumber);
+  content += `<li>${outputNumber}</li>`;
   
 }
+
+content += `</ul>`;
+
+myContainerContent.innerHTML = content;
