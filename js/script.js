c67b520722d6fc75344fb8081dc4b2130fcae559
Fernweh99 <daniele.dimento@gmail.com>
2022-06-23 14:51:38 +0200

    Create number 1 to 100

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+//dischiaro variabile fuori dal for per raccogliere tutti i numeri
+let content = "";
+
+//creo ciclo per creare i numeri e farci validazioni e operazioni
+for (let i = 1 ; i <= 100 ; i++) {
+  const number = i;
+  console.log(number);
+}
